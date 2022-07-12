f654f4c2f23db8695e72c6a9664146e0d7821dfe
edqbao <edqbao@gmail.com>
2022-07-12 15:27:56 -0400

    remove encode chunkced

--- suds/transport/https.py
+++ suds/transport/https.py
@@ -59,7 +59,7 @@ class HttpAuthenticated(HttpTransport):
 
     def open(self, request):
         self.addcredentials(request)
-        return HttpTransport.open(self, request, encode_chunked=False)
+        return HttpTransport.open(self, request)
 
     def send(self, request):
         self.addcredentials(request)
